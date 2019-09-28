5d847f9cfcba72b993e44449602e9c750c687f4e
diegobalde <diegobalderrama93@gmail.com>
2019-09-28 02:05:34 -0300

    Add project files.

---
+++ WinFormswithEFSample/Form1.cs
@@ -0,0 +1,14 @@
+﻿// https://docs.microsoft.com/en-us/ef/ef6/fundamentals/databinding/winforms
+
+using System.Windows.Forms;
+
+namespace WinFormswithEFSample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
